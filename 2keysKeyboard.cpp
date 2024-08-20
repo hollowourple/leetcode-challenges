e4628eccc449dbfeadb987deb2d1020beb642b4c
Anushka <155999721+hollowourple@users.noreply.github.com>
2024-08-20 16:41:19 +0530

    Create 2keysKeyboard.cpp

---
+++ 2keysKeyboard.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        if(n==0 || n==1) return 0;
+        for(int i =2;i<=sqrt(n);i++){
+            if(n%i==0){
+                return i + minSteps(n/i);
+            }
+        }
+        return n;
+    }
+};
