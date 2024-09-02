ef1e98be9b51098c65aff59a29a317bcb3d5b254
Anushka <155999721+hollowourple@users.noreply.github.com>
2024-09-02 13:40:34 +0530

    FindTheStudentToReplaceChalk.cpp
    There are n students in a class numbered from 0 to n - 1. The teacher will give each student a problem starting with the student number 0, then the student number 1, and so on until the teacher reaches the student number n - 1. After that, the teacher will restart the process, starting with the student number 0 again.
    
    You are given a 0-indexed integer array chalk and an integer k. There are initially k pieces of chalk. When the student number i is given a problem to solve, they will use chalk[i] pieces of chalk to solve that problem. However, if the current number of chalk pieces is strictly less than chalk[i], then the student number i will be asked to replace the chalk.
    
    Return the index of the student that will replace the chalk pieces.

---
+++ FindTheStudentToReplaceChalk.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int n= chalk.size();
+        long long s =0;
+        for(int i=0;i<n;i++){
+            s+=chalk[i];
+        }
+        k%=s;
+        for(int i=0;i<n;i++){
+            if(k<chalk[i]) return i;
+            k=k-chalk[i];
+        }
+        return 0;
+    }
+};
